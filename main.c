e1e9618b8f35770179ff6e09b39f7b79d4dba413
Fabrizio Fadigati <f4berack@proton.me>
2025-11-13 19:40:01 +0100

    Added first draft /authorize endpoint

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <microhttpd.h>
 
 #define PORT 8080
@@ -9,11 +10,40 @@ int handler(void *cls, struct MHD_Connection *conn,
             const char *ver, const char *upload_data,
             size_t *upload_data_size, void **con_cls)
 {
-    const char *msg = "test";
+    char *msg = malloc(strlen("test")+1);
+    strcpy(msg, "test");
+
+    if (strcmp(url, "/authorize") == 0) {
+
+        const char *client_id = MHD_lookup_connection_value(conn, MHD_GET_ARGUMENT_KIND, "client_id");
+        const char *redirect_uri = MHD_lookup_connection_value(conn, MHD_GET_ARGUMENT_KIND, "redirect_uri");
+        const char *response_type = MHD_lookup_connection_value(conn, MHD_GET_ARGUMENT_KIND, "response_type");
+        const char *state = MHD_lookup_connection_value(conn, MHD_GET_ARGUMENT_KIND, "state");
+
+        printf("client_id=%s, redirect_uri=%s, response_type=%s, state=%s\n",
+               client_id ? client_id : "(null)",
+               redirect_uri ? redirect_uri : "(null)",
+               response_type ? response_type : "(null)",
+               state ? state : "(null)");
+
+        free(msg);
+        msg = strdup("authorize");
+        if (!msg) {
+            fprintf(stderr, "Memory allocation failed\n");
+            return MHD_NO;
+        }
+    }
+
     struct MHD_Response *resp = MHD_create_response_from_buffer(strlen(msg),
                                                                 (void*)msg,
                                                                 MHD_RESPMEM_MUST_FREE);
-    return MHD_queue_response(conn, MHD_HTTP_OK, resp);
+    
+    int response = MHD_queue_response(conn, MHD_HTTP_OK, resp);
+    
+    MHD_destroy_response(resp);
+
+    return response;
+
 }
 
 int main()
